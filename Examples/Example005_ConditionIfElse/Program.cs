6798a47961931012aa18c3fa3b80a2b36d2cda1e
Igor <romanovis@gmail.com>
2022-07-08 15:25:00 +0300

    add Example 005 Condition If Else  project

---
+++ Examples/Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Please, enter your name: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha")
+ {
+    Console.WriteLine("Wow, it's Masha!");
+ }
+ else
+ {
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+ }
