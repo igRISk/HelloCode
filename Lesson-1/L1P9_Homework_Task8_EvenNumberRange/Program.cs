2ceaf0b85dc30a6d28adb434f83b37daef811a67
Igor <romanovis@gmail.com>
2022-07-09 23:46:49 +0300

    Add gitignore

--- Lesson-1/L1P9_Homework_Task8_EvenNumberRange/Program.cs
+++ Lesson-1/L1P9_Homework_Task8_EvenNumberRange/Program.cs
@@ -1,7 +1,7 @@
 ﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
 // 
 // 5 -> 2, 4
-// 8 -> 2, 4, 6, 8
+// 8 -> 2, 4, 6, 8 
 
 Console.Write("Enter a number: ");
 int num = int.Parse(Console.ReadLine());
@@ -10,6 +10,7 @@ Console.Write(num + " -> ");
 
 if (num % 2 != 0) num = num - 1;
 int i = 2;
+
 while (i <= num) 
 {
     Console.Write(i + " ");
