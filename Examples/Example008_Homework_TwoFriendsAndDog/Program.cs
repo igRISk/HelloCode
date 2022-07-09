a4b1a332b4898bb176d35347e8c889b8e0cf9f44
Igor <romanovis@gmail.com>
2022-07-09 13:32:30 +0300

    add Example 008 Homework: 'Two friends and dog' project

---
+++ Examples/Example008_Homework_TwoFriendsAndDog/Program.cs
@@ -0,0 +1,29 @@
+﻿int count = 0;
+int distance = 10000;
+int firstFriendSpeed = 1;
+int secondFriendSpeed = 2;
+int dogSpeed = 5;
+int friend = 2;
+int time = 0;
+
+while (distance > 10)
+ {
+    if (friend == 1) 
+    {
+        time = distance / (firstFriendSpeed + dogSpeed);
+        friend = 2;
+    }
+    else
+    {
+        time = distance / (firstFriendSpeed + dogSpeed);
+        friend = 1;
+    }
+    distance = distance - (firstFriendSpeed + secondFriendSpeed) * time;
+//    Console.Write("Distance = ");
+//    Console.WriteLine(distance);
+    count++;
+ }
+
+Console.Write("The dog will run ");
+Console.Write(count);
+Console.WriteLine(" times");
