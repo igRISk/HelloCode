3710fad0fd55d27e00d0046c24a85f87ebf1b087
Igor <romanovis@gmail.com>
2022-07-10 19:29:53 +0300

    Add new examples from lesson 2

---
+++ Examples/Example010_MethodArray/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array= { 1, 12, 31, 4, 18, 15, 4, 43, 55 };
+
+int n = array.Length;
+Console.Write("Please enter a number: ");
+int find = int.Parse(Console.ReadLine());
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine("Element index in array = " + index);
+        break;
+    }
+    index++;
+}
---
+++ Examples/Example011_ArrayLibrary/Program.cs
@@ -0,0 +1,50 @@
+﻿void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray (int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    Console.Write("Your array: { ");
+    while (position < count)
+    {
+        Console.Write(col[position] + " ");
+        position++;
+    }
+    Console.Write("}");
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int num = 4;
+int pos = IndexOf(array, num);
+Console.Write("Index num '" + num + "' in array: " + pos);
